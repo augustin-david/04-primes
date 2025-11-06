a105b6a3eaea41296c72ed5549898179daa287a3
augustin-david <augustin.david@edu.esiee.fr>
2025-11-06 08:39:36 +0000

    Ajout d'un module pour déterminer et afficher les nombres premiers inférieurs à 100.

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+"""
+Module permettant de determiner des nombres premiers inférieurs à 100.
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
     # votre code ici
-
-    pass
+    """
+    Retourne True si p est premier, False sinon.
+    """
+    if p < 2:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Applique la fonction secondaire pour afficher les nombres premiers inférieurs à 100.
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
